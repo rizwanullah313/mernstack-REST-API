a8ba9529f9d04c1a03a4ea9280ec7e338a85dd48
rizwanullah313 <ru323091@gmail.com>
2022-04-16 12:07:34 -0700

    get response done

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,13 @@
 
 const express = require('express')
 const dotenv = require('dotenv').config()
-const port = 5000
+const port = process.env.PORT || 5000
 
 const app = express()
+
+app.get('/api/goals', (req, res) => {
+    //res.send('Get Goals')
+    res.status(200).json({message: 'Get Goals'})
+})
 
 app.listen(port, ()=> console.log(`Server Started on Port ${port}`))
