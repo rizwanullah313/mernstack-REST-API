5a87731e7ee808d1754d1c83f08e301a03f3b50b
rizwanullah313 <ru323091@gmail.com>
2022-04-16 11:56:04 -0700

    initilize the port and listen

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,12 @@
 //npm i express mongoose colors dotenv
 // npm i -D nodemon
 
-console.log("Hello backend Server .js");
+//console.log("Hello backend Server .js");
+
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = 5000
+
+const app = express()
+
+app.listen(port, ()=> console.log(`Server Started on Port ${port}`))
