ea80073032870e0cda7b8d9d9c5d5c0c95b7e18e
rizwanullah313 <ru323091@gmail.com>
2022-04-16 14:33:25 -0700

    successfully connect to mongoDB

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,13 @@
 //console.log("Hello backend Server .js");
 
 const express = require('express')
+const colors = require('colors')
 const dotenv = require('dotenv').config()
 const {errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
+
+connectDB()
 
 const app = express()
 
