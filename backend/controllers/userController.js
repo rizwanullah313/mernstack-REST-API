fbd063a92fc3c0c809775a9ec5c1e6bcd662cd21
rizwanullah313 <ru323091@gmail.com>
2022-04-25 16:42:37 -0700

    Login User Successfully

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -51,7 +51,23 @@ const registerUser = asyncHandler(async (req, res) => {
 
 // for Authentication also POST and Public
 const loginUser = asyncHandler(async (req, res) => {
-    res.json({message: 'Login User'})
+    const {email, password} = req.body
+    
+    // Check for user Email
+    const user = await User.findOne({email})
+
+    if(user && (await bcrypt.compare(password, user.password))){
+        res.json({
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+        })
+    }else{
+       res.status(400)
+       throw new Error('Invalid Credentials')
+    }
+
+   // res.json({message: 'Login User'})
 })
 
 
